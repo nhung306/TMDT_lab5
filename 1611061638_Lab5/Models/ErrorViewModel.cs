b0acfae2df946a550a82c64c756d2c95406ac3c0
NhungThanh <tnhung306@gmail.com>
2020-01-03 12:54:56 +0700

    mi

---
+++ 1611061638_Lab5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _1611061638_Lab5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
